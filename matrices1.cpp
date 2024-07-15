6531ee6bba56be067434d5d68b77d23bb3a50d14
jhosep_unjbg <jhosep.aro2005@gmail.com>
2024-07-15 10:55:11 -0500

    sube primer ejercicio matrices

---
+++ matrices1.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main(){
+	
+	int n;
+	int m;
+	float Matriz1[m][n];
+	
+	cout<<"ingrese el orden de filas la matriz:";
+	cin>>m;
+	cout<<"ingrese el orden de columnas de la matriz";
+	cin>>n;
+	
+	cout<<Matriz1[m][n];
+		return 0;
+	
+	
+}
